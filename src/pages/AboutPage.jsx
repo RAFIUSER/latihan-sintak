3b647ccf2bf66bd66675ae26a5550eef88560a80
Rafiuser <rafiussyakban@gmail.com>
2025-05-27 15:34:19 +0700

    fix issue

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -3,21 +3,13 @@ import NavBar from "../components/navbar";
 import Footer from "../components/footer";
 import RafiNameCard from "../components/name-card/rafi";
 import Namecard from "../components/name-card/nizel";
-=======
-
 import MuteaNamecard from "../components/name-card/mutea";
-
 import FazelNameCard from "../components/name-card/fazel";
-
 import AisyahNameCard from "../components/name-card/aisyah";
-
-
 import DindaNamecard from "../components/name-card/dinda";
 import AyuNameCard from "../components/name-card/ayu";
 import Fajar from "../components/name-card/fajar";
 import AndinNameCard from "../components/name-card/andin";
-
-
 
 function AboutPage() {
   return (
@@ -25,32 +17,17 @@ function AboutPage() {
       <NavBar />
       <div className="min-h-screen py-24 ">
         <div className="flex gap-8 flex-wrap items-center justify-center w-full px-4">
-        
-        <RafiNameCard />
+          <RafiNameCard />
 
-        {/* Panggil kartu nama kamu dibawah sini..... */}
-        <Namecard />
-=======
-        <MuteaNamecard />
-
-        <FazelNameCard/>
-
-
-        <AisyahNameCard />
-
-
-
-        <DindaNamecard />
-
-
-<AyuNameCard/>
-
-        <Fajar />
-        <AndinNameCard/>
-
-
-
-
+          {/* Panggil kartu nama kamu dibawah sini..... */}
+          <Namecard />
+          <MuteaNamecard />
+          <FazelNameCard />
+          <AisyahNameCard />
+          <DindaNamecard />
+          <AyuNameCard />
+          <Fajar />
+          <AndinNameCard />
         </div>
       </div>
       <Footer />
